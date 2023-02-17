e51e85b58fc386d855946717cc052d3124f469b0
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-02-17 18:45:21 -0500

    Add tecnologies icons

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
 import styles from './Main.module.css'
 import javaScriptIcon from '../../assets/icons8-javascript-96.svg'
 import reactIcon from '../../assets/react.svg'
 import reduxIcon from '../../assets/icons8-redux.svg'
+import cssIcon from '../../assets/icons8-css3.svg'
+import nodeIcon from '../../assets/icons8-nodejs.svg'
+import postgresIcon from '../../assets/icons8-postgresql.svg'
 
 export const Main = () => {
   return (
@@ -21,13 +25,20 @@ export const Main = () => {
               alta calidad y estoy emocionado por el futuro del desarrollo web.
             </h2>
             <div className={styles['Main-Buttons']}>
-              <button>Proyectos</button>
-              <button>Contactame</button>
+              <Link to={'/proyects'}>
+                <button>Proyectos</button>
+              </Link>
+              <Link to={'/contact'}>
+                <button>Contactame</button>
+              </Link>
             </div>
             <div className={styles['Main-Icons']}>
-                <img src={javaScriptIcon} alt="JavaScript" />
-                <img src={reactIcon} alt="React" />
-                <img src={reduxIcon} alt="Redux" />
+              <img src={javaScriptIcon} alt="JavaScript" />
+              <img src={reactIcon} alt="React" />
+              <img src={reduxIcon} alt="Redux" />
+              <img src={cssIcon} alt="Css" />
+              <img src={nodeIcon} alt="Node" />
+              <img src={postgresIcon} alt="Postgres" />
             </div>
           </div>
         </section>
